39bab92959ed4eb2fef94bc95650ba7b3dbbbf4c
Varsha-m1 <varshapramila@gmail.com>
2021-06-05 12:34:19 -0700

    Primitive fault tolerance test

--- network/network_test.go
+++ network/network_test.go
@@ -27,19 +27,6 @@ func as(cond bool) {
 	}
 }
 
-/*func TestInitialize (t *testing.T) {
-	fmt.Printf("Starting Server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        er(err)
-    }
-	var c Peer.Config
-	c.Peer = 0
-	c.addr.append("http://localhost:8080")
-	Peer.initialize(c)
-
-}
-*/
-
 // -addr=localhost:3000
 
 func TestReadPeer(t *testing.T) {
@@ -64,20 +51,33 @@ func TestReadPeer(t *testing.T) {
 
 	log.Println(Peer2.Rga.GetView())
 	as(Peer2.Rga.Contains(elem))
+}
 
-	//var buf bytes.Buffer
-	/*
-		//var msg Message
-		//e := crdt.Elem{ID: crdt.Id{0, 0, 0}, After: crdt.Id{}, Rem: crdt.Id{}, Val: 8}
-		//msg := Message{e: e, vc: crdt.VecClock{}}
-		//enc := gob.NewEncoder(&buf)
-		//enc.Encode(msg)
-		//err := c.WriteMessage(websocket.TextMessage, buf.Bytes())
-		if err != nil {
-			er(err)
-		}
-		err1 := readPeer(c)
-		ne(err1)
-	*/
+func TestFaultTolerance(t *testing.T) {
+	addrs := []string{"localhost:8080", "localhost:8081"}
+	config := Config{
+		Peer:  0,
+		Addrs: addrs,
+	}
+	Peer1 := MakePeer(config)
+	config.Peer = 1
+	Peer2 := MakePeer(config)
+
+	go Peer1.Serve()
+	go Peer2.Serve()
+	Peer1.InitPeer()
+	Peer2.InitPeer()
+
+	Peer1.dc = true
+	elem, err := Peer1.Rga.Append('1', crdt.Id{})
+	ne(err)
+	elem1, err1 := Peer1.Rga.Append('r', crdt.Id{})
+	elem_r, err_r := Peer1.Rga.Remove(elem.ID)
+	ne(err_r)
+	Peer1.dc = false
+	time.Sleep(2 * time.Second)
+
+	log.Println(Peer2.Rga.GetView())
+	as(Peer2.Rga.Contains(elem))
 
 }
